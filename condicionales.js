2d3396a9ab293748d0c774d84ebba203860ea7c2
Angel <angel.ramirezg@hotmail.com>
2020-12-07 15:06:38 -0300

    commit_JavascriptCondicional_v2

--- condicionales.js
+++ condicionales.js
@@ -2,11 +2,25 @@
 //     codigo
 // } 
 
-var edad = 17;
+// var edad = 17;
 
-if (edad  >= 18){
-    document.write("Eres mayor de edad");
+// if (edad  >= 18){
+//     document.write("Eres mayor de edad");
 
-}else{
-    document.write("Eres menor de edad");
+// }else{
+//     document.write("Eres menor de edad");
+// }
+
+var pais = 'España';
+// if (pais == "Mexico"){
+//     document.write("Eres mexico");
+// }else{
+//     document.write("No eres mexico")
+// }
+
+switch(pais){
+
+    case 'España':
+        document.write("Eres español");  
+    break;
 }
