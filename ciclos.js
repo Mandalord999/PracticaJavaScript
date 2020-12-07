4eef1d11b9f184c3d901259d071f935a8ae7a00d
Angel <angel.ramirezg@hotmail.com>
2020-12-07 18:00:46 -0300

    commit_JavascriptCiclos_v2

--- ciclos.js
+++ ciclos.js
@@ -1,4 +1,14 @@
-for (var i = 0; i < 10; i++){
-document.write(i + " ");
+// for (var i = 0; i < 10; i++){
+// document.write(i + " ");
+// }
 
-}
+var meses = ['Enero' , 'Febrero' , 'Marzo' , 'Abril' , 'Mayo' , 'Junio' , 'Julio' , 'Agosto' , 'Septiembre' , 'Octubre' , 'Noviembre' , 'Diciembre'];
+
+// for(var i = 0; i < meses.length; i++){
+//     document.write(meses[i]);
+// }
+
+// for (mes in meses){
+//     document.write(meses[mes]);
+// }
+
