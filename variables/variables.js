412d1f568e71f2edf73cff2567419fe9a38ef6b7
Angel <angel.ramirezg@hotmail.com>
2020-12-07 12:16:04 -0300

    commit_Javascript_v7

--- variables/variables.js
+++ variables/variables.js
@@ -1,6 +1,7 @@
 var nombre = 'heeeeeeeeeeey!!! Come On!!!';
 alert(nombre);
 
-var numero2 = 2;
-var numero1 = 1;
+var numero2 = 100;
+var numero1 = 87;
 alert(numero1 + numero2);
+document.write(numero1);
