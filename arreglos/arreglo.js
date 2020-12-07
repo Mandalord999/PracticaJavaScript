2a5fcf635478c53f80deaf1a612befaf43fa1955
Angel <angel.ramirezg@hotmail.com>
2020-12-07 14:11:35 -0300

    commit_JavascriptArreglos_v3

--- arreglos/arreglo.js
+++ arreglos/arreglo.js
@@ -3,6 +3,7 @@
 
 var meses = ['Enero' , 'Febrero' , 'Marzo' , 'Abril' , 'Mayo' , 'Junio' , 'Julio' , 'Agosto' , 'Septiembre' , 'Octubre' , 'Noviembre' , 'Diciembre'];
 
+document.write (meses [3]);
 
 
 
