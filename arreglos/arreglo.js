09d43a6fbfe863045db3293b13d36066a94fcfd5
Angel <angel.ramirezg@hotmail.com>
2020-12-07 14:15:27 -0300

    commit_JavascriptArreglos_v4

--- arreglos/arreglo.js
+++ arreglos/arreglo.js
@@ -2,8 +2,18 @@
 // var amigos = [ 'Carlos' , 'Arturo' , 'Cesar' , 123 , true];
 
 var meses = ['Enero' , 'Febrero' , 'Marzo' , 'Abril' , 'Mayo' , 'Junio' , 'Julio' , 'Agosto' , 'Septiembre' , 'Octubre' , 'Noviembre' , 'Diciembre'];
+ 
+var amigos = [];
+amigos[0] = "Alejandro";
+amigos[1] = "Matias";
+amigos[2] = "Carola";
+amigos[3] = "Camila";
 
-document.write (meses [3]);
+// document.write (meses [3]);
+
+console.log (meses);
+
+console.log (amigos);
 
 
 
