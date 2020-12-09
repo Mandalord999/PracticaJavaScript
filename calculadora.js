6697b0ebbedbbb0b97789ff92fec2fd087a8ca93
Angel <angel.ramirezg@hotmail.com>
2020-12-09 14:21:30 -0300

    commit_JavascriptCalculadora_v1

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+
+function suma (){
+    var numero1=document.getElementById('numero1').value;
+    var numero2=document.getElementById('numero2').value;
+    var resultado= numero1 + numero2;
+    document.getElementById('resultado').value = resultado;
+}
+
+
+
+
