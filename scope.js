9bd5f373709c621dc7776e600c06b06437912e1e
Angel <angel.ramirezg@hotmail.com>
2020-12-09 15:25:48 -0300

    commit_JavascriptScope_v2

--- scope.js
+++ scope.js
@@ -1,7 +1,15 @@
-function saludo(){
-    var texto = 'Hola Mundo';
-    return texto;
-}
+// function saludo(){
+//     var texto = 'Hola Mundo';
+//     return texto;
+// }
 
+// saludo();
+// console.log(saludo());
+
+function saludo(){
+    texto = 'Hola Mundo';
+    
+    //texto es una variable global dado que no tiene la plabra "var"
+}
 saludo();
-console.log(saludo());
+console.log(texto);
