fcd3b9c1bcdcf9e005fab25442f3868b0259320b
Angel <angel.ramirezg@hotmail.com>
2020-12-16 08:42:31 -0300

    commit_JavascriptCadenas_v3

--- cadenas.js
+++ cadenas.js
@@ -3,8 +3,8 @@ var nombre = "Carlos Arturo";
 // var segundoNombre = nombre.substring(7,13);
 // var segundoNombre = nombre.substr(7,2);
 var nuevoNombre = nombre.replace('Arturo','Heeeeeeeeeey');
-document.write(nuevoNombre);
+
+// document.write(nuevoNombre);
 // document.write(numeroCaracteres);
 // document.write(nombre + " tiene " + nombre.length + " caracteres.");
-
-document.write(segundoNombre); 
+// document.write(segundoNombre); 
