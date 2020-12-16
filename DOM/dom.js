e200293f7789da2ce46e6c4663e847ad56a456f4
Angel <angel.ramirezg@hotmail.com>
2020-12-16 09:14:03 -0300

    commit_JavascriptDOM_v2

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,4 +1,7 @@
-var cajas = document.getElementsByTagName('div');
+// var cajas = document.getElementsByTagName('div');
+// var cajas = document.getElementsByClassName('caja');
+var primeracaja = document.getElementById('primeracaja');
 
+primeracaja.textContent = 'Heeeey!!';
+// cajas[0].innerHTML = '<h1> Hey!!! </h1>';
 
-cajas[0].textContent = 'Heeeey!!';
