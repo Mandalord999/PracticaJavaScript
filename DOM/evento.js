34e6f9fe089d9dd1b7a98c44d72fa41ec68ed331
Angel <angel.ramirezg@hotmail.com>
2020-12-16 12:04:04 -0300

    commit_Javascriptevento_v3

--- DOM/evento.js
+++ DOM/evento.js
@@ -1,6 +1,21 @@
-function saludo(){
-    alert('Hola');
+// function saludo(){
+//     alert('Hola');
+// }
+
+function crearCaja(){
+    var contenedor = document.getElementById('contenedor');
+    var caja = document.createElement('div');
+    caja.className = 'caja';
+    contenedor.appendChild(caja);
 }
 
 var btn = document.getElementById('btn');
-btn.addEventListener('click' , saludo);
+
+btn.addEventListener('click' , crearCaja);
+
+// Funciones anonimas
+
+//     btn.addEventListener('click' , function(){
+//     CODIGO A EJECUTAR
+//     });
+
