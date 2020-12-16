cd4e3e073fc55e5f14ccec06dd22385ca5fac6a8
Angel <angel.ramirezg@hotmail.com>
2020-12-16 13:25:17 -0300

    commit_JavascriptFormulario_v1

---
+++ DOM/formulario.js
@@ -0,0 +1,27 @@
+var formulario = document.getElementById('formulario');
+var nombre = formulario.nombre;
+var sexo = formulario.sexo;
+var terminos = formulario.terminos;
+
+function validar(e){
+
+    // Valimadmos el nombre
+    if(nombre.length > 18){
+        alert('Maximo de caracteres permitidos');
+        
+    }else if(nombre.value == ''){
+        alert('Por favor ingresa un nombre');
+
+    }
+
+
+
+
+
+
+    e.preventDefault();
+}
+
+formulario.addEventListener('submit', validar);
+
+
