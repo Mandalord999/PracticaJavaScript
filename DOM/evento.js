b6740a9f1e933853f347441edb4bb81623ef8103
Angel <angel.ramirezg@hotmail.com>
2020-12-16 12:28:50 -0300

    commit_Javascriptevento_v4

--- DOM/evento.js
+++ DOM/evento.js
@@ -2,20 +2,28 @@
 //     alert('Hola');
 // }
 
-function crearCaja(){
-    var contenedor = document.getElementById('contenedor');
-    var caja = document.createElement('div');
-    caja.className = 'caja';
-    contenedor.appendChild(caja);
-}
+// function crearCaja(){
+//     var contenedor = document.getElementById('contenedor');
+//     var caja = document.createElement('div');
+//     caja.className = 'caja';
+//     contenedor.appendChild(caja);
+// }
 
-var btn = document.getElementById('btn');
+// var btn = document.getElementById('btn');
 
-btn.addEventListener('click' , crearCaja);
+// btn.addEventListener('click' , crearCaja);
 
 // Funciones anonimas
 
 //     btn.addEventListener('click' , function(){
 //     CODIGO A EJECUTAR
 //     });
+var cajas = document.getElementsByClassName('caja');
+
+function color(){
+    cajas[0].classList.toggle('amarillo');
+}
+
+cajas[0].addEventListener('click' , color);
+
 
